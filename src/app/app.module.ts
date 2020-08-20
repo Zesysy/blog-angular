841beb50201d309721a256118fa3ac85f220ebec
sylene <sylene.manusset@gmail.com>
2020-08-20 15:25:10 +0200

    generate two components: one for the list of posts and one for the posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostListItemComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
